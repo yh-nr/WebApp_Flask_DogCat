26f53f04d4ed67a089108821780c02645f21eddc
yh-nr <ynaru911@gmail.com>
2023-05-31 00:37:08 +0900

    3rd commit

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ def predict(img):
     # ネットワークの準備
     net = Net().cpu().eval()
     # # 学習済みモデルの重み（dog_cat.pt）を読み込み
-    net.load_state_dict(torch.load('./src/dog_cat.pt', map_location=torch.device('cpu')))
+    # net.load_state_dict(torch.load('./src/dog_cat.pt', map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load('./dog_cat.pt', map_location=torch.device('cpu')))
     #　データの前処理
     img = transform(img)
     img =img.unsqueeze(0) # 1次元増やす
